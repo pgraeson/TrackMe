c088316f7c7626ac40c1203a63c344bf6e66f06d
pgraeson <pgraeson@deakin.edu.au>
2018-08-09 10:23:38 +0200

    New update

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const devices = [];
-devices.push({ user: "Mary", name: "Mary's iPhone" });
-devices.push({ user: "Alex", name: "Alex's Surface Pro" });
-devices.push({ user: "Mary", name: "Mary's MacBook" });
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
 devices.forEach(function(device) {
     $('#devices tbody').append(`
@@ -15,7 +12,9 @@ devices.forEach(function(device) {
    $('#add-device').on('click', function() {
     const user = $('#user').val();
     const name = $('#name').val();
+    
     devices.push({ user, name });
+    localStorage.setItem('devices', JSON.stringify(devices));
     location.href = 'device-list.html'; 
    });
    
