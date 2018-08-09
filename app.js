e2266d2ae798563cb3bcd62172067a07bddc54f8
pgraeson <pgraeson@deakin.edu.au>
2018-08-09 09:10:11 +0200

    APP

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const devices = [];
+devices.push({ user: "Mary", name: "Mary's iPhone" });
+devices.push({ user: "Alex", name: "Alex's Surface Pro" });
+devices.push({ user: "Mary", name: "Mary's MacBook" });
 
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`
+    );
+   });
