15f886b75305c15dc1b5019c95ea6714bd5b9d78
pgraeson <pgraeson@deakin.edu.au>
2018-08-09 23:45:20 +0200

    New Update: extra feautures added

--- app.js
+++ app.js
@@ -17,4 +17,10 @@ devices.forEach(function(device) {
     localStorage.setItem('devices', JSON.stringify(devices));
     location.href = 'device-list.html'; 
    });
+
+   $('#send-command').on('click', function() {
+    const command = $('#command').val();
+    console.log(`command is: ${command}`);
+   });
+  
    
