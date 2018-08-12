5095b7c0b3ed1af98962ce7a4e3929b12130a562
pgraeson <pgraeson@deakin.edu.au>
2018-08-12 09:20:33 +0200

    NEW: work

---
+++ web/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+const base = `${__dirname}/public`;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {res.sendFile(`${base}/device-list.html`);});
+
+app.get('*', (req, res) => {res.sendFile(`${base}/404.html`);});
+
+app.listen(port, () => { console.log(`listening on port ${port}`);});
