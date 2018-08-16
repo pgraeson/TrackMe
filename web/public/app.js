a9344a987b34d5a366046364afd2aa395cefa3f6
pgraeson <pgraeson@deakin.edu.au>
2018-08-16 22:40:35 +0200

    NEW UPDATE OF FILES

---
+++ web/public/app.js
@@ -0,0 +1,28 @@
+
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`
+    );
+   });
+
+   $('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    
+    devices.push({ user, name });
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = '/'; 
+   });
+
+   $('#send-command').on('click', function() {
+    const command = $('#command').val();
+    console.log(`command is: ${command}`);
+   });
+   
+  $('#navbar').load('navbar.html');
+   
--- web/server.js
+++ web/server.js
@@ -8,12 +8,13 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {res.sendFile(`${base}/device-list.html`);});
 
-app.get('*', (req, res) => {res.sendFile(`${base}/404.html`);});
-
 app.get('/register-device', (req, res) => {res.sendFile(`${base}/register-device.html`);});
 
 app.get('/send-command', (req, res) => {res.sendFile(`${base}/send-command.html`);});
     
 app.get('/about', (req, res) => {res.sendFile(`${base}/about-me.html`);});
+
+app.get('*', (req, res) => {res.sendFile(`${base}/404.html`);});
+
 
 app.listen(port, () => { console.log(`listening on port ${port}`);});
